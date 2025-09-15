4dd452cf4b0aaadd532d8abce9887d52300b3e4a
nicsosof <nicososatrigo@gmail.com>
2025-09-14 22:18:56 -0300

    b

--- js/config/app.js
+++ js/config/app.js
@@ -36,25 +36,51 @@ function mostrarUIUsuario(nombre)
 function cargarSolicitudes() {
     const solicitudesCol = collection(db, "solicitudes");
 
-    // Suscripción en tiempo real
-    onSnapshot(solicitudesCol, (querySnapshot) => {
-        container.innerHTML = ""; // limpiar antes de renderizar
+    onAuthStateChanged(auth, async (user) => {
+        if (user) {
+            try {
+                const uid = user.uid;
+                const userRef = doc(db, "usuarios", uid);
+                const userSnap = await getDoc(userRef);
+                if (userSnap.exists()) {
+                    const data = userSnap.data();
 
-        querySnapshot.forEach((doc) => {
-            const data = doc.data();
-            const div = document.createElement("div");
-            div.classList.add("solicitud");
-            div.innerHTML = `
-            <h3>${data.servicio || "Desconocido"}</h3>
-            <p>${data.nombre}</p>
-            <p><strong>Urgencia: </strong>${data.urgencia}</p>
-            <p><strong>Ubicación: </strong>${data.ubicacion}</p>
-            <p><strong>Horario: </strong>${data.hora}</p>
-            <p><strong>Descripción:</strong> ${data.comentario || "Sin descripción"}</p>
-            <button>Tomar</button>
-            `;
-            container.appendChild(div);
-        });
+                    if (data.rol === "user")
+                    {
+                        alert("No tienes permisos.")
+                        return;
+                    }
+                    
+                    // Suscripción en tiempo real
+                    onSnapshot(solicitudesCol, (querySnapshot) => {
+                        container.innerHTML = ""; // limpiar antes de renderizar
+
+                        querySnapshot.forEach((doc) => {
+                            const data = doc.data();
+                            const div = document.createElement("div");
+                            div.classList.add("solicitud");
+                            div.innerHTML = `
+                            <h3>${data.servicio || "Desconocido"}</h3>
+                            <p>${data.nombre}</p>
+                            <p><strong>Urgencia: </strong>${data.urgencia}</p>
+                            <p><strong>Ubicación: </strong>${data.ubicacion}</p>
+                            <p><strong>Horario: </strong>${data.hora}</p>
+                            <p><strong>Descripción:</strong> ${data.comentario || "Sin descripción"}</p>
+                            <button>Tomar</button>
+                            `;
+                            container.appendChild(div);
+                        });
+                    });
+
+                } else {
+                    console.log("No existe el documento del usuario");
+                }
+            } catch (error) {
+                console.error("Error al obtener usuario:", error);
+            }
+        } else {
+            alert("Necesitas estar logueado.");
+        }
     });
 }
 
